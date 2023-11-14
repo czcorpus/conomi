32c5019c08404dabb23fc8a331cefd4d1664b915
Tomas Machalek <tomas@machalek.xyz>
2023-11-14 12:29:47 +0100

    In Zulip messages, show different emoji for different severity levels

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE conomi_reports (
     id int(11) NOT NULL AUTO_INCREMENT,
     app varchar(50) NOT NULL,
     instance varchar(50),
-    tag varchar(50),
+    tag varchar(100),
     severity varchar(50) NOT NULL,
     subject text NOT NULL,
     body text NOT NULL,
